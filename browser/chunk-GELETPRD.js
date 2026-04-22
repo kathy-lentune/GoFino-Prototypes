46fc5dd7190c23a50b1f768b4965cae384e80c3e
KathyKok <kathy@lentune.com>
2026-04-22 17:19:23 +1200

    deploy prototypes

---
+++ browser/chunk-GELETPRD.js
@@ -0,0 +1,1 @@
+import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./chunk-C62U5QPR.js").then(o=>o.UserAccessComponent)}];export{t as USER_ACCESS_ROUTES};
---
+++ browser/chunk-PMD5DHTW.js
@@ -0,0 +1,1 @@
+import"./chunk-C6Q5SG76.js";var t=[{path:"",loadComponent:()=>import("./chunk-GU75WPLN.js").then(o=>o.SettingsDashboardComponent)}];export{t as SETTINGS_ROUTES};
